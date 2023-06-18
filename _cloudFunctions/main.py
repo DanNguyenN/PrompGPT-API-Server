c075bd4a38dd5fcadf550105f0dce8710b94bff0
Dan Nguyen <ndan0112@gmail.com>
2023-06-18 00:28:57 -0500

    Queue worked

--- _cloudFunctions/main.py
+++ _cloudFunctions/main.py
@@ -1,67 +1,67 @@
 import base64
 
-import uuid
 import json
-from google.cloud import pubsub_v1
 
 from predict import returnPrediction
 from cloudevents.http import CloudEvent
 import functions_framework
 
-project_id = "aerobic-gantry-387923"
-topic_id = "promptgpt-result"
-publisher = pubsub_v1.PublisherClient()
-subscriber = pubsub_v1.SubscriberClient()
+from bson import json_util
 
-subscription_path = "projects/aerobic-gantry-387923/subscriptions/promptgpt-sub"
+from google.api_core.exceptions import FailedPrecondition
+
+from pymongo.mongo_client import MongoClient
+from collections.abc import MutableMapping
+uri = "mongodb+srv://ndan0112:mqbgMMhH1i2lyD2t@promptgpt.zkajoet.mongodb.net/?retryWrites=true&w=majority"
+
+
+
 
 # Triggered from a message on a Cloud Pub/Sub topic.
 @functions_framework.cloud_event
 def subscribe(cloud_event: CloudEvent) -> None:
+    # Create a new client and connect to the server
+    client = MongoClient(uri)
+    # Init the collection
+    db = client['predictions']
+    collection = db['predictions_collection']
     # Print out the data from Pub/Sub, to prove that it worked
     # Decode the Base64-encoded data from Pub/Sub message
     data = base64.b64decode(cloud_event.data["message"]["data"]).decode("utf-8")
     print("received data", data) #received data {"messages": [{"data": "abcd2", "attributes": {"token": "c0e0dddf-bbfc-4390-8fff-a21d1f008fa9"}}]}
     
+    
     # Extract the token from Pub/Sub message attributes
     token = json.loads(data)["messages"][0]["attributes"]["token"]
     text = json.loads(data)["messages"][0]["data"]
 
+    
+    existToken = collection.find_one({'token': token})
+    if existToken:
+        if existToken['prediction'] != "not ready":
+            print("token already exists and is already processed")
+            return ("", 204)
+    else:
+        return("", 204)
     # Process the data and get the prediction
-    prediction = returnPrediction(text)#it return {"predictions": [{"revised": answer}]}
-    print(prediction)
+    
+    try:
+        prediction = returnPrediction(text)#it return {"predictions": [{"revised": answer}]}
+        print(prediction)
+    except FailedPrecondition as error:
+        collection.update_one(
+        {'token': token},
+        {'$set': {'prediction': "Vertex AI Server is down. Try again later!"}}
+        )
+        return ("", 204)
     #get revised answer
 
-    # Create a new message with the prediction and token
-    results = {
-        "messages": [
-            {
-                "data": prediction,
-                "attributes": {
-                    "token": token
-                }
-            }
-        ]
-    }
 
-    # Publish the result to a new Pub/Sub topic
-    topic_path = publisher.topic_path(project_id, topic_id)
-    future = publisher.publish(topic_path, data=json.dumps(results).encode("utf-8"))
+    collection.update_one(
+        {'token': token},
+        {'$set': {'prediction': prediction}}
+    )
 
-    # Wait for the message to be published
-    future.result()
-
-    # #  Acknowledge the message
-    # print(data)
-    # ack_ids = json.loads(data)["ackId"]
-    # try:    
-    #     subscriber.acknowledge(request={
-    #         'subscription': subscription_path,
-    #         'ack_ids': ack_ids,
-    #     })
-    # except:
-    #     print("Acknowledge Failed")
-    #     pass
 
     # # Acknowledge the Pub/Sub message
     return ("", 204)
--- app/main.py
+++ app/main.py
@@ -17,7 +17,15 @@ from google.cloud import pubsub_v1
 # Python program for the above approach.
 import collections
 
+from bson import json_util
 
+from pymongo.mongo_client import MongoClient
+uri = "mongodb+srv://ndan0112:mqbgMMhH1i2lyD2t@promptgpt.zkajoet.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri)
+# Init the collection
+db = client['predictions']
+collection = db['predictions_collection']
 
 project_id = "aerobic-gantry-387923"
 topic_id = "promptgpt"
@@ -45,7 +53,7 @@ app.add_middleware(
 
 def generate_token():
     # Define the length of the token
-    token_length = 16
+    token_length = 32
 
     # Define the characters to use in the token
     characters = string.ascii_letters + string.digits
@@ -75,6 +83,9 @@ async def predict(text: str):
             }
         ]
     }
+
+    collection.insert_one({'token': token, 'input': text,'prediction': "not ready"})
+
     #publish to server pubsub
     future = publisher.publish(topic_path, data=json.dumps(data).encode("utf-8"))
     
@@ -82,44 +93,28 @@ async def predict(text: str):
     future.result()
 
 
-    print("Prediction published to Pub/Sub.")
+    print("Request published to Pub/Sub.")
     return {"token": token}
 
 @app.get("/result/{token}")
 async def result(token: str):
-
-    response = subscriber.pull(receivePRequest)
-
-    print(response)
-
-    for received_message in response.received_messages:
-        print(received_message)
-        # Extract the message data and attributes
-        data = received_message.message.data.decode("utf-8")
+    result = collection.find_one({'token': token})
+    if result: 
+        if result['prediction'] == "not ready":
+            query = {"prediction": "not ready"}
+            #Get the index of result
+            results = collection.find(query)
+            document_list = list(results)
+            result_index = document_list.index(result)
+            # Get the length of the results
+            result_count = collection.count_documents(query)
+            return {"position": result_index + 1, "queueSize": result_count}
         
-        
-        message_token = str(json.loads(data)["messages"][0]["attributes"]["token"])
-        
-        print(token)
-        print(message_token)
-        print(token == message_token)
-
-        # Process the message data and perform necessary operations
-        if token == message_token:
-            result = json.loads(data)["messages"][0]["data"]
-            try: 
-                subscriber.acknowledge(request={
-                    'subscription': subscription_path,
-                    'ack_ids': received_message.ack_id,
-                })
-            except:
-                print("Acknowledge error")
-                pass
-            return result
-        
-        # Acknowledge the message to mark it as processed
+        return json.loads(json_util.dumps(result))
+    else:
+        raise HTTPException(status_code=404, detail="Invalid Token")
     
-    return {"prediction": "not ready", "token": token}
+    
         
 
 if __name__ == "__main__":
